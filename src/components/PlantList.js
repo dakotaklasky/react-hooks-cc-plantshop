48305381f5b8319463bc9fd346a2bb97ec589cd9
Dakota <dakota.klasky@gmail.com>
2024-08-13 12:37:54 -0400

    required deliverables

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,14 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
+  
+  const plantComponents = plants.map(plant => {
+    return <PlantCard key={plant.id} plant={plant}></PlantCard>
+})
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantComponents}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+const API_URL = "http://localhost:6001/plants"
+
+const [plants, setPlants] = useState([])
+const [searchText, setSearchText] = useState("")
+
+useEffect(() => {
+    fetch(API_URL)
+    .then(response => response.json())
+    .then(plantData => setPlants(plantData))
+  }, [])
+
+const filteredPlants = plants.filter(plants => {
+
+    if(plants.name !== undefined){
+      return plants.name.toUpperCase().includes(searchText.toUpperCase())  
+    }
+    else{
+      return false
+    }
+    
+})
+
+function addPlant(newPlant){
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "Application/JSON"
+    },
+    body:JSON.stringify(newPlant)
+  })
+  .then(response => response.json())
+  .then(newPlantData => setPlants([...plants,newPlantData]))
+}
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant = {addPlant}/>
+      <Search searchText={searchText} setSearchText={setSearchText} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
